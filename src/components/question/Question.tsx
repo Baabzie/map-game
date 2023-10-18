171362a1e3794e1c8ad929e3548bf8a1d0d1de4d
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2023-10-18 14:52:36 +0200

    new questions

--- src/components/question/Question.tsx
+++ src/components/question/Question.tsx
@@ -19,11 +19,6 @@ const jsonData: LocationData[] = [
     longitude: 18.08001291554731
   },
   {
-    questionSwe: "Gunnarsons Specialkonditori",
-    latitude: 59.31006212870785,
-    longitude: 18.075216544332868
-  },
-  {
     questionSwe: "Globen",
     latitude: 59.29367385346533,
     longitude: 18.083496455658846
@@ -38,7 +33,48 @@ const jsonData: LocationData[] = [
     latitude: 59.299573847468494,
     longitude: 18.076899228039238
   },
+  {
+    questionSwe: "Ringen",
+    latitude: 59.30863938774631,
+    longitude: 18.07577284253091
+  },
+  {
+    questionSwe: "Allhelgonakyrkan",
+    latitude: 59.3098901485267,
+    longitude: 18.07064490325645
+  },
+  {
+    questionSwe: "Åsö vuxengymnasium",
+    latitude: 59.311521237061235,
+    longitude: 18.074291302508367
+  },
+  {
+    questionSwe: "Skrapan",
+    latitude: 59.31213017098195,
+    longitude: 18.073909564691057
+  },
+  {
+    questionSwe: "Göta Lejon",
+    latitude: 59.31360742628512,
+    longitude: 18.073916644956448
+  },
+  {
+    questionSwe: "Sofia kyrka",
+    latitude: 59.31250129852528,
+    longitude: 18.088798678690033
+  },
+  {
+    questionSwe: "Eriksdalsbadet",
+    latitude: 59.30510635605385,
+    longitude: 18.07520797636408
+  },
+  {
+    questionSwe: "Högalidskyrkan",
+    latitude: 59.3178066330842,
+    longitude: 18.0377022869954
+  },
 ];
+
 
 function calculateDistance(lat1: number, lon1: number, lat2: number, lon2: number): number {
   const R = 6371; // Radius of the Earth in kilometers
