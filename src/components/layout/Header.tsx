23d1524fc03059d46a8650a71af8115e09b78a29
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2023-10-18 14:24:19 +0200

    test

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header: React.FC = () => {
     return (
         <header>
-            <h1>Map Game</h1>
+            <h1>Kartspelet</h1>
         </header>
     );
 }
