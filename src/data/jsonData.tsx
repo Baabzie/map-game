7ca2fad604db1eafff3b3c653e7abd77ab18e8bf
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2023-10-23 13:04:22 +0200

    updated temporary data

---
+++ src/data/jsonData.tsx
@@ -0,0 +1,78 @@
+interface LocationData {
+    questionSwe: string;
+    longitude: number;
+    latitude: number;
+  }
+
+export const jsonData: LocationData[] = [
+  {
+    questionSwe: "Nackas Hörna",
+    latitude: 59.31129173009845,
+    longitude: 18.08001291554731
+  },
+  {
+    questionSwe: "Globen",
+    latitude: 59.29367385346533,
+    longitude: 18.083496455658846
+  },
+  {
+    questionSwe: "Södersjukhuset",
+    latitude: 59.30993246128399,
+    longitude: 18.055870424815247
+  },
+  {
+    questionSwe: "Värmdö gymnasium",
+    latitude: 59.299573847468494,
+    longitude: 18.076899228039238
+  },
+  {
+    questionSwe: "Ringen",
+    latitude: 59.30863938774631,
+    longitude: 18.07577284253091
+  },
+  {
+    questionSwe: "Allhelgonakyrkan",
+    latitude: 59.3098901485267,
+    longitude: 18.07064490325645
+  },
+  {
+    questionSwe: "Åsö vuxengymnasium",
+    latitude: 59.311521237061235,
+    longitude: 18.074291302508367
+  },
+  {
+    questionSwe: "Skrapan",
+    latitude: 59.31213017098195,
+    longitude: 18.073909564691057
+  },
+  {
+    questionSwe: "Göta Lejon",
+    latitude: 59.31360742628512,
+    longitude: 18.073916644956448
+  },
+  {
+    questionSwe: "Sofia kyrka",
+    latitude: 59.31250129852528,
+    longitude: 18.088798678690033
+  },
+  {
+    questionSwe: "Eriksdalsbadet",
+    latitude: 59.30510635605385,
+    longitude: 18.07520797636408
+  },
+  {
+    questionSwe: "Högalidskyrkan",
+    latitude: 59.3178066330842,
+    longitude: 18.0377022869954
+  },
+  {
+    questionSwe: "Café Eurobar",
+    latitude: 59.309934187783654,
+    longitude: 18.078978881612034
+  },
+  {
+    questionSwe: "Adolf Fredriks kyrka",
+    latitude: 59.337903803257355,
+    longitude: 18.060059393093184
+  },
+];
